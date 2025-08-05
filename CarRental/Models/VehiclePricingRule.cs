06a5e722641a43a67f4071d1f95f2ddf3a7cfc23
DucDN <ducdn2@rikkeisoft.com>
2025-08-05 15:58:04 +0700

    Add vehicle Pring rule class

---
+++ CarRental/Models/VehiclePricingRule.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarRental.Models;
+
+[Table("vehicle_pricing_rules")]
+public class VehiclePricingRule
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("vehicle_id")]
+    public Guid VehicleId { get; set; }
+
+    [Column("price_per_day")]
+    public decimal PricePerDay { get; set; }
+
+    [Column("weekend_multiplier")]
+    public double WeekendMultiplier { get; set; } = 1.0;
+
+    [Column("holiday_multiplier")]
+    public double HolidayMultiplier { get; set; } = 1.0;
+
+    [Column("effective_date")]
+    public DateTime EffectiveDate { get; set; }
+
+    [Column("expiry_date")]
+    public DateTime ExpiryDate { get; set; }
+}
